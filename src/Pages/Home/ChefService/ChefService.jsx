f666146478752d384545f972015c97d2ac619c57
sakib <s.sarder232002@gmail.com>
2023-05-23 05:17:48 +0600

    homework done with chef service section

---
+++ src/Pages/Home/ChefService/ChefService.jsx
@@ -0,0 +1,18 @@
+import "./ChefService.css";
+const ChefService = () => {
+  return (
+    <div className="bg-white text-center  flex justify-center items-center  h-[40vh] chef-service-bg">
+      <div className="bg-white py-5 px-12 rounded-md space-y-3  w-3/4">
+        <h1 className="text-4xl font-bold">Bistro Boss</h1>
+        <p className="text-sm">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus, libero accusamus laborum deserunt ratione dolor
+          officiis praesentium! Deserunt magni aperiam dolor eius dolore at,
+          nihil iusto ducimus incidunt quibusdam nemo.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ChefService;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
 import ChefRecommends from "../ChefRecommends/ChefRecommends";
+import ChefService from "../ChefService/ChefService";
 import Featured from "../Featured/Featured";
 import PopularMenu from "../PopularMenu/PopularMenu";
 import Testimonials from "../Testimonials/Testimonials";
@@ -10,6 +11,7 @@ const Home = () => {
     <div className="px-4 md:px-0">
       <Banner></Banner>
       <Category></Category>
+      <ChefService></ChefService>
       <PopularMenu></PopularMenu>
       <ChefRecommends></ChefRecommends>
       <Featured></Featured>
